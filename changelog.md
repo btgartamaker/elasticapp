b5c9e729e0600f18562fdec708c5813998aea14b
btgartamaker <btgartamaker@gmail.com>
2020-05-28 20:13:00 -0500

    Added additional functions. See changelog

---
+++ changelog.md
@@ -0,0 +1,2 @@
+# Change Log
+- 5/28 8:05p - added funtionality for filtering on time and size. Started work on the aggregates functions. Need to review time filtering
--- es-query.py
+++ es-query.py
@@ -1,18 +1,37 @@
 from elasticsearch import Elasticsearch
+import json
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-S", "--server", help="defines ELK server to query")
+parser.add_argument("-S", "--server", help="defines ELK server to query", default="localhost")
 parser.add_argument("-i", "--index", help="defines index to query")
-parser.add_argument("-f", "--field", help="defines the field to print")
+parser.add_argument("-f", "--field", help="defines the field to print", default="none")
+parser.add_argument("-t", "--time", help="defines the amount of time to look back at", default="15m")
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument("-s", "--size", help="defines the number of records to pull", default=0)
+group.add_argument("-a", "--aggregate", help="defines what aggregation to use")
+
 args = parser.parse_args()
 
 server = args.server
 log_index = args.index
 field =  args.field
+time = args.time
+doc_size = int(args.size)
+
+def print_size(data, filter):
+  for f in data["hits"]["hits"]:
+    if field == "none":
+      json_object = f["_source"]
+      json_formatted_str = json.dumps(json_object, indent=2)
+      print(json_formatted_str)
+    else:
+      print(f["_source"][filter])
 
 
 es = Elasticsearch(hosts=[server])
+
+#print(time)
 
 query_body = {
   "query": {
@@ -29,7 +48,7 @@ query_body = {
             {
               "range": {
                 "@timestamp": {
-                    "from": "now-5m",
+                    "from": f"now-{time}",
                     "to":  "now"
                 }
               }
@@ -42,8 +61,10 @@ query_body = {
   }
 }
 
-response = es.search(index=log_index, body=query_body, size="15")
+response = es.search(index=log_index, body=query_body, size=doc_size)
+#print(response)
 
-
-for f in response["hits"]["hits"]:
-    print(f["_source"][field])
+if doc_size > 0:
+  print_size(response, field)
+elif doc_size == 0:
+  print("Function on the way")
